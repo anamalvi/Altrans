78607d4bd96dd32d519ca55ee542e8bc6c9706e0
anamalvi <silver_lolly_1@hotmail.com>
2014-12-27 02:50:59 -0500

    Tried adding the search bar
    - search bar not showing predictions
    - js not reading input from search bar
    - map.js:40 giving an error in chrome dev console

--- map.js
+++ map.js
@@ -15,6 +15,7 @@ var transportFactors =
 
 function initialize() 
 {
+	// Rendering Directions & Times
 	directionsDisplay = new google.maps.DirectionsRenderer();
 	
 	var mapOptions = 
@@ -31,7 +32,51 @@ function initialize()
 	computeTotalDistance(directionsDisplay.directions);
 	computeTotalTime(directionsDisplay.directions);  
 	});
+	
+	// Search Bar
+	var markers = [];
+    var input = (document.getElementById('pac-input'));
+	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+	var searchBox = new google.maps.places.SearchBox(input);
+    google.maps.event.addListener(searchBox, 'places_changed', function() {
+    var places = searchBox.getPlaces();
+	
+	//don't currently understand what this if statement is really doing
+    //if (places.length == 0) {
+    //  return;
+    //}
+	
+    for (var i = 0, marker; marker = markers[i]; i++) {
+      marker.setMap(null);
+    }
 
+    // For each place, get the icon, place name, and location.
+    markers = [];
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0, place; place = places[i]; i++) {
+      var image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location
+      });
+
+      markers.push(marker);
+
+      bounds.extend(place.geometry.location);
+    }
+
+    map_one.fitBounds(bounds);
+  });
 }
 
 function calcRoute() 
